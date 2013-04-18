5d9cd8c9875fe48f03d31bb4b9afb3a2ae75eb22
Daniel Warneke <warneke@icsi.berkeley.edu>
2013-04-17 18:33:48 -0700

    Started to switch to memory-mapped I/O

---
+++ cdfs-daemon/src/main/java/edu/berkeley/icsi/cdfs/sharedmem/SharedMemoryConsumer.java
@@ -0,0 +1,80 @@
+package edu.berkeley.icsi.cdfs.sharedmem;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileChannel.MapMode;
+
+import edu.berkeley.icsi.cdfs.cache.BufferPool;
+import edu.berkeley.icsi.cdfs.utils.NumberUtils;
+
+public final class SharedMemoryConsumer {
+
+	private final DatagramSocket socket;
+
+	private final DatagramPacket notificationPacket;
+
+	private final DatagramPacket ackPacket;
+
+	private final RandomAccessFile memoryMappedFile;
+
+	private final MappedByteBuffer sharedMemoryBuffer;
+
+	private boolean bufferReady = true;
+
+	public SharedMemoryConsumer(final DatagramSocket socket) throws IOException {
+		this.socket = socket;
+
+		// Create notification packet
+		final byte[] buf = new byte[256];
+		this.notificationPacket = new DatagramPacket(buf, buf.length);
+
+		// Receive information about memory mapped file
+		socket.receive(this.notificationPacket);
+
+		final int bufSize = NumberUtils.byteArrayToInteger(buf, 0);
+		final String filename = new String(buf, 4, this.notificationPacket.getLength() - 4);
+
+		this.memoryMappedFile = new RandomAccessFile(filename, "r");
+		final FileChannel fc = this.memoryMappedFile.getChannel();
+
+		this.sharedMemoryBuffer = fc.map(MapMode.READ_ONLY, 0, BufferPool.BUFFER_SIZE);
+		this.sharedMemoryBuffer.limit(bufSize);
+
+		// Create acknowledgment packet
+		final byte[] ackBuf = new byte[1];
+		this.ackPacket = new DatagramPacket(ackBuf, ackBuf.length);
+		this.ackPacket.setSocketAddress(this.notificationPacket.getSocketAddress());
+	}
+
+	public ByteBuffer lockSharedMemory() throws IOException {
+
+		if (!this.bufferReady) {
+			this.socket.receive(this.notificationPacket);
+			final int bufferSize = NumberUtils.byteArrayToInteger(this.notificationPacket.getData(), 0);
+			if (bufferSize == -1) {
+				return null;
+			}
+			this.sharedMemoryBuffer.position(0);
+			this.sharedMemoryBuffer.limit(bufferSize);
+			this.bufferReady = true;
+		}
+
+		return this.sharedMemoryBuffer;
+	}
+
+	public void unlockSharedMemory() throws IOException {
+
+		this.bufferReady = false;
+		this.socket.send(this.ackPacket);
+	}
+
+	public void close() throws IOException {
+
+		this.memoryMappedFile.close();
+	}
+}
---
+++ cdfs-daemon/src/main/java/edu/berkeley/icsi/cdfs/sharedmem/SharedMemoryProducer.java
@@ -0,0 +1,111 @@
+package edu.berkeley.icsi.cdfs.sharedmem;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileChannel.MapMode;
+
+import edu.berkeley.icsi.cdfs.cache.BufferPool;
+import edu.berkeley.icsi.cdfs.utils.NumberUtils;
+
+public final class SharedMemoryProducer {
+
+	private final DatagramSocket socket;
+
+	private final SocketAddress remoteAddress;
+
+	private DatagramPacket notificationPacket;
+
+	private final DatagramPacket ackPacket;
+
+	private RandomAccessFile memoryMappedFile = null;
+
+	private MappedByteBuffer sharedMemoryBuffer = null;
+
+	private boolean bufferReady;
+
+	public SharedMemoryProducer(final SocketAddress remoteAddress) throws IOException {
+
+		this.socket = new DatagramSocket();
+		this.remoteAddress = remoteAddress;
+
+		// Prepare acknowledgment packet
+		final byte[] ackBuf = new byte[1];
+		this.ackPacket = new DatagramPacket(ackBuf, ackBuf.length);
+
+		this.bufferReady = true;
+	}
+
+	private ByteBuffer getBuffer() throws IOException {
+
+		if (this.memoryMappedFile == null) {
+
+			final String filename = getTempFilename();
+
+			// Create notification buffer;
+			final byte[] filenameBuf = filename.getBytes();
+			final byte[] notificationBuf = new byte[filenameBuf.length + 4];
+			System.arraycopy(filenameBuf, 0, notificationBuf, 4, filenameBuf.length);
+			this.notificationPacket = new DatagramPacket(notificationBuf, notificationBuf.length);
+			this.notificationPacket.setSocketAddress(this.remoteAddress);
+			this.memoryMappedFile = new RandomAccessFile(filename, "rw");
+			final FileChannel fc = this.memoryMappedFile.getChannel();
+			this.sharedMemoryBuffer = fc.map(MapMode.READ_WRITE, 0, BufferPool.BUFFER_SIZE);
+		}
+
+		return this.sharedMemoryBuffer;
+	}
+
+	private static String getTempFilename() {
+
+		return "/tmp/daniel_1234";
+	}
+
+	public ByteBuffer lockSharedMemory() throws IOException {
+
+		if (!this.bufferReady) {
+			this.socket.receive(this.ackPacket);
+			this.bufferReady = true;
+		}
+
+		final ByteBuffer buf = getBuffer();
+		buf.clear();
+
+		return buf;
+	}
+
+	public void unlockSharedMemory() throws IOException {
+
+		if (this.sharedMemoryBuffer == null) {
+			throw new IllegalStateException("Shared memory buffer has never been locked before");
+		}
+
+		this.sharedMemoryBuffer.flip();
+
+		// Update size in notification packet
+		byte[] buf = this.notificationPacket.getData();
+		NumberUtils.integerToByteArray(this.sharedMemoryBuffer.limit(), buf, 0);
+
+		// Send notification
+		this.socket.send(this.notificationPacket);
+
+		this.bufferReady = false;
+	}
+
+	public void close() throws IOException {
+
+		// Update size in notification packet
+		byte[] buf = this.notificationPacket.getData();
+		NumberUtils.integerToByteArray(-1, buf, 0);
+
+		this.socket.send(this.notificationPacket);
+
+		this.socket.close();
+		this.memoryMappedFile.close();
+	}
+}
