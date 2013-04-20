599783c258766ff391f0df29c181a051477ebd31
Daniel Warneke <warneke@icsi.berkeley.edu>
2013-04-19 18:24:43 -0700

    Removed old I/O classes

--- cdfs-daemon/src/main/java/edu/berkeley/icsi/cdfs/datanode/AbstractReadOp.java
+++
@@ -1,9 +0,0 @@
-package edu.berkeley.icsi.cdfs.datanode;
-
-import java.io.IOException;
-import java.net.SocketAddress;
-
-abstract class AbstractReadOp {
-
-	abstract void read(final SocketAddress remoteAddress) throws IOException;
-}
