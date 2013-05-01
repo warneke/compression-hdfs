8aaca89f81b481e7635de8dbbde50da750875332
Daniel Warneke <warneke@icsi.berkeley.edu>
2013-05-01 14:27:31 -0700

    Improved statistics presentation

--- cdfs-wlgen/src/main/java/edu/berkeley/icsi/cdfs/wlgen/Statistics.java
+++ cdfs-wlgen/src/main/java/edu/berkeley/icsi/cdfs/wlgen/Statistics.java
@@ -8,7 +8,7 @@ import java.util.TreeMap;
 public final class Statistics {
 
 	private static final double TO_GB = 1024.0 * 1024.0 * 1024.0;
-	
+
 	private static final class HistogramEntry {
 
 		private int numberOfFiles = 0;
@@ -23,7 +23,8 @@ public final class Statistics {
 
 		final Map<Integer, HistogramEntry> histogram = new TreeMap<Integer, HistogramEntry>();
 		final Iterator<File> it = files.iterator();
-		long totalAmountOfData = 0L;
+		long totalAmountOfGeneratedData = 0L;
+		long totalAmountOfReadData = 0L;
 		while (it.hasNext()) {
 
 			final File file = it.next();
@@ -31,7 +32,7 @@ public final class Statistics {
 			if (numberOfInputUsages.intValue() == 0) {
 				continue;
 			}
-			
+
 			HistogramEntry entry = histogram.get(numberOfInputUsages);
 			if (entry == null) {
 				entry = new HistogramEntry();
@@ -39,7 +40,8 @@ public final class Statistics {
 			}
 			++entry.numberOfFiles;
 			entry.totalAmountOfData += file.getUncompressedFileSize();
-			totalAmountOfData += file.getUncompressedFileSize();
+			totalAmountOfGeneratedData += file.getUncompressedFileSize();
+			totalAmountOfReadData += (file.getUncompressedFileSize() * file.getNumberOfInputUsages());
 		}
 
 		final Iterator<Map.Entry<Integer, HistogramEntry>> it2 = histogram.entrySet().iterator();
@@ -47,12 +49,61 @@ public final class Statistics {
 
 			final Map.Entry<Integer, HistogramEntry> entry = it2.next();
 			final HistogramEntry he = entry.getValue();
-			final double dataInGB = ((double) he.totalAmountOfData/ TO_GB);
-			final float percent = (float) he.totalAmountOfData / (float) totalAmountOfData * 100.0f;
-			System.out.println(entry.getKey() + ":\t" + he.numberOfFiles + " (" +dataInGB + ", " + percent + "%)");
+			final float generatedAmountInGB = (float) ((double) he.totalAmountOfData / TO_GB);
+			final float percentGeneratedData = (float) he.totalAmountOfData / (float) totalAmountOfGeneratedData
+				* 100.0f;
+			final float readAmountInGB = (float) ((double) he.totalAmountOfData * entry.getKey().intValue() / TO_GB);
+			final float percentReadData = (float) he.totalAmountOfData * entry.getKey().intValue()
+				/ (float) totalAmountOfReadData * 100.0f;
+			printLine(entry.getKey().intValue(), he.numberOfFiles, generatedAmountInGB, percentGeneratedData,
+				readAmountInGB, percentReadData);
 
 		}
-		
-		System.out.println("Total amount of data generated: " + ((double) totalAmountOfData/ TO_GB) + " GB");
+
+		final float totalGeneratedInGB = (float) ((double) totalAmountOfGeneratedData / TO_GB);
+		final float totalReadInGB = (float) ((double) totalAmountOfReadData / TO_GB);
+		System.out.println("Total generated " + String.format("%.2f", totalGeneratedInGB) + " GB, total read "
+			+ String.format("%.2f", totalReadInGB) + " GB");
+	}
+
+	private static void printLine(final int numberOfInputUsages, final int numberOfIndividualFiles,
+			final float amountOfGeneratedDataInGB, final float percentGeneratedData, final float amountOfReadDataInGB,
+			final float percentReadData) {
+
+		final StringBuffer sb = new StringBuffer();
+
+		sb.append(padString(Integer.toString(numberOfInputUsages), 5));
+		sb.append('|');
+		sb.append(padString(Integer.toString(numberOfIndividualFiles), 5));
+		sb.append('|');
+		sb.append(padString(String.format("%.2f", amountOfGeneratedDataInGB), 9));
+		sb.append('|');
+		sb.append(padString(String.format("%.1f", percentGeneratedData), 5));
+		sb.append('%');
+		sb.append('|');
+		sb.append(padString(String.format("%.2f", amountOfReadDataInGB), 9));
+		sb.append('|');
+		sb.append(padString(String.format("%.1f", percentReadData), 5));
+		sb.append('%');
+		sb.append('|');
+
+		System.out.println(sb.toString());
+	}
+
+	private static String padString(final String str, final int width) {
+
+		final StringBuffer sb = new StringBuffer(Math.max(str.length(), width));
+
+		int pad = width - str.length();
+		while (pad > 0) {
+
+			sb.append(' ');
+
+			--pad;
+		}
+
+		sb.append(str);
+
+		return sb.toString();
 	}
 }
